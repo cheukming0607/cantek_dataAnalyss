e02c76235a3d35f67434891839215b763cba710d
cheukming0607 <aucheukming06071999@gmail.com>
2023-10-17 15:34:50 -0400

    Updated query_01.sql with question

--- Query_01/query_01.sql
+++ Query_01/query_01.sql
@@ -1,4 +1,8 @@
 -- 1. Comparison operator '>'
+-- Q: Which films replacement cost larger than $20? 
+-- 	Shows the table with the film ID, title, and replacement cost, 
+-- 	descending order on the cost.
+
 
 SELECT film_id, title, replacement_cost
 FROM film
